4b8633721fc424bd6a39428c62e8ab51003d6d79
serubi <serubi@live.com>
2018-09-18 14:32:45 +0200

    Tilføjet summaries og lavet rettelser

--- OresundBilletLibrary/OresundBil.cs
+++ OresundBilletLibrary/OresundBil.cs
@@ -5,13 +5,23 @@ namespace OresundBilletLibrary
 {
     public class OresundBil : Bil
     {
-        // Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nummerplade">Nummerplade på max 7 tegn</param>
+        /// <param name="dato">Dato køretøjet kørte over broen</param>
         public OresundBil(string nummerplade, DateTime dato) : base(nummerplade, dato)
         {
             Nummerplade = nummerplade;
             Dato = dato;
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nummerplade">Nummerplade på max 7 tegn</param>
+        /// <param name="dato">Dato køretøjet kørte over broen</param>
+        /// <param name="brobizz">Om Brobizz blev brugt til betaling</param>
         public OresundBil(string nummerplade, DateTime dato, bool brobizz) : base(nummerplade, dato, brobizz)
         {
             Nummerplade = nummerplade;
@@ -19,11 +29,20 @@ namespace OresundBilletLibrary
             Brobizz = brobizz;
         }
 
+        /// <summary>
+        /// Get price for vehicle
+        /// </summary>
+        /// <returns></returns>
         public override decimal Pris()
         {
             return Brobizz ? 161 : 410;
         }
 
+        /// <summary>
+        /// Navnet på køretøjstypen.
+        /// Default er "Øresund Bil".
+        /// </summary>
+        /// <returns></returns>
         public override string Køretøj()
         {
             return "Øresund Bil";
--- OresundBilletLibrary/OresundMotorcykel.cs
+++ OresundBilletLibrary/OresundMotorcykel.cs
@@ -5,13 +5,23 @@ namespace OresundBilletLibrary
 {
     public class OresundMotorcykel : Motorcykel
     {
-        // Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nummerplade">Nummerplade på max 7 tegn</param>
+        /// <param name="dato">Dato køretøjet kørte over broen</param>
         public OresundMotorcykel(string nummerplade, DateTime dato) : base(nummerplade, dato)
         {
             Nummerplade = nummerplade;
             Dato = dato;
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nummerplade">Nummerplade på max 7 tegn</param>
+        /// <param name="dato">Dato køretøjet kørte over broen</param>
+        /// <param name="brobizz">Om Brobizz blev brugt til betaling</param>
         public OresundMotorcykel(string nummerplade, DateTime dato, bool brobizz) : base(nummerplade, dato, brobizz)
         {
             Nummerplade = nummerplade;
@@ -19,11 +29,20 @@ namespace OresundBilletLibrary
             Brobizz = brobizz;
         }
 
+        /// <summary>
+        /// Get price for vehicle
+        /// </summary>
+        /// <returns></returns>
         public override decimal Pris()
         {
             return Brobizz ? 73 : 210;
         }
 
+        /// <summary>
+        /// Navnet på køretøjstypen.
+        /// Default er "Øresund MC".
+        /// </summary>
+        /// <returns></returns>
         public override string Køretøj()
         {
             return "Øresund MC";
